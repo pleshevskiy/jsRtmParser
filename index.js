c7d0dd3545c642c197d3b7bc79f080f087971cbb
Pleshevskiy Dmitriy <pleshevskiy@gmail.com>
2016-04-16 10:23:29 +0300

    Переработаны конфиги. Конфиг начинается с символа @ # теперь означает комментарии и игнорируются парсером
    Теперь их 3 вида:
    - Глобальные
    - Метода
    - Маршрута
    
    Новый тип значения конфига - RegExp
    в паттерны можно записывать @p.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var $ = require('njs-utils');
 
 var path = require('path');
 var fs   = require('fs');
@@ -8,14 +9,22 @@ var rl   = require('readline');
 /** @const Шаблоны для парсинга файла */
 var patterns = {
 
+    regExp: /^\/(.+)\/([ig]*)$/,
+
+    /** @regexp Парсинг пустых строк */
+    empty: /^\s+$/,
+
+    /** @regexp Парсинг комментариев */
+    comments: /^#/,
+
     /** @regexp Парсинг методов */
-    method: /^(GET|POST|PUT|DELETE):\s*$/gm,
+    method: /^(GET|POST|PUT|DELETE):\s*$/,
 
     /** @regexp Парсинг конфигурации */
-    config: /^#\s*(\w+)\s*=\s*(.+)$/gm,
+    config: /^(\s*)(@\w*?)\.(\w+)\s*=\s*(.+)$/,
 
     /** @regexp Парсинг ссылок и событий*/
-    route: /^([^\s]+)\s+([^\s]+?)$/gm
+    route: /^(\s*)([^\s]+)\s+([^\s]+?)$/
 };
 
 
@@ -29,53 +38,109 @@ function RtmParser(filepath) {
 }
 
 
+$.extend(RtmParser.prototype, {
+    /**
+     * @public
+     * Парсит файл .rtm
+     */
+    parse: function (hooks) {
+        hooks || (hooks = {});
 
-/**
- * @public
- * Парсит файл .rtm
- */
-RtmParser.prototype.parse = function (callback) {
-    var self = this;
+        var self = this;
+        var lineReader = rl.createInterface({
+            input: fs.createReadStream(this.filepath)
+        });
 
-    var lineReader = rl.createInterface({
-        input: fs.createReadStream(this.filepath)
-    });
+        var currentRoute;
+        var globalConfig = {}, methodConfig, currentConfig;
+        var preventRouteWhiteSpace = '';
 
-    var currentConfig;
-    var currentRoute;
+        lineReader.on('line', function (line) {
+            var match;
 
-    lineReader.on('close', function () {
-        if (typeof callback === 'function') {
-            callback.call(self);
+            if (!line.length || patterns.empty.test(line) || patterns.comments.test(line)) {
+                // Пропустить линию, если она пуста или это комментарий
+                return;
+            }
+
+            if (match = patterns.method.exec(line)) {
+                self.rtmList.push({
+                    method: match[1],
+                    routes: []
+                });
+
+                methodConfig = {};
+                currentRoute = self.rtmList[self.rtmList.length - 1];
+
+            } else if (match = patterns.config.exec(line)) {
+                var _config;
+
+                if (currentRoute == null) {
+                    _config = globalConfig;
+                } else if (preventRouteWhiteSpace.length && preventRouteWhiteSpace <= match[1]) {
+                    _config = currentConfig;
+                } else {
+                    _config = methodConfig;
+                }
+
+                var _key = match[3];
+                var _value = parseRtmConfigValue_(match[4]);
+
+                switch (match[2]) {
+                    case '@': {
+                        _config[_key] = _value;
+                        break;
+                    }
+                    case '@p': {
+                        _config.patterns || (_config.patterns = {});
+                        _config.patterns[_key] = _value;
+                        break;
+                    }
+                }
+
+            } else if (match = patterns.route.exec(line)) {
+                currentConfig = $.extend({}, methodConfig, globalConfig);
+                preventRouteWhiteSpace = match[1];
+
+                currentRoute.routes.push({
+                    url: match[2],
+                    action: match[3],
+                    config: typeof hooks.setConfig === 'function' ? hooks.setConfig(currentConfig, currentRoute.method) : currentConfig
+                });
+            }
+        });
+
+        lineReader.on('close', function () {
+            if ($.isFunction(hooks.onClose)) {
+                hooks.onClose.call(self);
+            }
+        });
+
+        return this;
+    },
+
+    /**
+     * @public
+     * Конвертирует rtmList в rtmObject
+     *
+     * @return {object}
+     */
+    toObject: function () {
+        var rtmObject = {};
+
+        for (var i = 0, max = this.rtmList.length; i < max; ++i) {
+            var rtmPart = this.rtmList[i];
+
+            if (rtmObject[rtmPart.method] == null) {
+                rtmObject[rtmPart.method] = [];
+            }
+
+            [].push.apply(rtmObject[rtmPart.method], rtmPart.routes);
         }
-    });
 
-    lineReader.on('line', function (line) {
-        var match;
-
-        if (match = patterns.method.exec(line)) {
-            self.rtmList.push({
-                method: match[1],
-                routes: []
-            });
-
-            currentConfig = {};
-            currentRoute = self.rtmList[self.rtmList.length - 1];
-
-        } else if (match = patterns.config.exec(line)) {
-            currentConfig[match[1]] = self.parseRtmConfigValue_(match[2]);
-
-        } else if (match = patterns.route.exec(line)) {
-            currentRoute.routes.push({
-                url: match[1],
-                action: match[2],
-                config: currentConfig
-            });
-        }
-    });
-
-    return this;
-};
+        return rtmObject;
+    }
+});
 
 /**
  * @private
@@ -83,38 +148,20 @@ RtmParser.prototype.parse = function (callback) {
  * @param {string} matchedValue Значение конфика спарсенное из конфинга .rtm
  * @return {string|boolean|int}
  */
-RtmParser.prototype.parseRtmConfigValue_ = function (matchedValue) {
-    var result;
+parseRtmConfigValue_ = function (matchedValue) {
+    var result, match;
 
     if (matchedValue === 'true' || matchedValue === 'false') {
         result = matchedValue === 'true';
     } else if (!isNaN(+matchedValue)) {
         result = +matchedValue;
+    } else if (match = patterns.regExp.test(matchedValue)) {
+        result = new RegExp(match[1], match[2]);
     } else {
         result = matchedValue;
     }
 
     return result;
-};
-
-/**
- * @public
- * Конвертирует rtmList в rtmObject
- */
-RtmParser.prototype.toObject = function () {
-    var rtmObject = {};
-
-    for (var i = 0, max = this.rtmList.length; i < max; ++i) {
-        var rtmPart = this.rtmList[i];
-
-        if (rtmObject[rtmPart.method] == null) {
-            rtmObject[rtmPart.method] = [];
-        }
-
-        [].push.apply(rtmObject[rtmPart.method], rtmPart.routes);
-    }
-
-    return rtmObject;
 };
 
 
